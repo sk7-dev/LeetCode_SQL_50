8945541f94a1176a3326df72c66c2d0a723ef61e
sk7-dev <karthee.shiv@gmail.com>
2026-01-12 13:55:35 -0800

    problem 35

---
+++ 35_1204_last_person_to_fit_in_the_bus.sql
@@ -0,0 +1,12 @@
+WITH t AS (
+  SELECT
+    person_name,
+    turn,
+    SUM(weight) OVER (ORDER BY turn) AS total_weight
+  FROM Queue
+)
+SELECT person_name
+FROM t
+WHERE total_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
